06d9b292912a7625c6ff1bae6e70b131e4f2f1c8
Khoi24 <Khoigrai1@gmail.com>
2021-12-29 08:49:05 +0700

    update

--- frame/mainFrame.java
+++ frame/mainFrame.java
@@ -1,6 +1,7 @@
 package frame;
 
 import javax.swing.*;
+import java.awt.*;
 import java.sql.SQLException;
 
 public class mainFrame extends JFrame{
@@ -14,9 +15,34 @@ public class mainFrame extends JFrame{
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
         JPanel jpTable = new tableModel();
+        JPanel jpInfo = new JPanel();
+        jpInfo.setBounds(10,200,500,400);
+        jpInfo.setLayout(null);
 
 
+        JLabel LID = new JLabel("ID");
+        LID.setBounds(10,10,200,30);
+        JLabel Lname = new JLabel("Name");
+        Lname.setBounds(10,50,200,30);
+        JLabel LDOB = new JLabel("DOB");
+        LDOB.setBounds(270,50,200,30);
+        JLabel Ladd = new JLabel("Address");
+        Ladd.setBounds(10,90,400,30);
+        JLabel Lstatus = new JLabel("Status");
+        Lstatus.setBounds(10,130,200,30);
+        JLabel Ltreatment = new JLabel("Treatment");
+        Ltreatment.setBounds(10,170,500,30);
+
+        jpInfo.add(Lname);
+        jpInfo.add(LDOB);
+        jpInfo.add(Ladd);
+        jpInfo.add(Lstatus);
+        jpInfo.add(Ltreatment);
+
+        this.add(jpInfo);
         this.add(jpTable);
+
+
         this.setVisible(true);
     }
 }
